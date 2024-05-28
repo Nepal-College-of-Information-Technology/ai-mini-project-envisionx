f45f90fdfedc5667236303e6e94c296e9117c509
aryyyn <unibixhi@gmail.com>
2024-05-28 19:41:53 +0545

    implemented authentication system

--- backend/ChatWave/Authentication/views.py
+++ backend/ChatWave/Authentication/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.http import JsonResponse
+import firebase_admin
+from firebase_admin import auth
 
-# Create your views here.
+def registerLogic(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        username = request.POST["username"]
+        
+        try:
+            user = auth.create_user(
+                email = email,
+                display_name = username,
+                password = password
+            )
+            return JsonResponse({'status': 'User created successfully'}, status=200)
+        
+        except Exception as err:
+            return JsonResponse({'status': 'Error creating user', 'error': str(err)}, status=400)
+    return render(request, 'register.html')
+
+
+
+def loginLogic(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        
+        try:
+            user = auth.get_user_by_email(email)
+            return JsonResponse({'status': 'User logged in successfully'}, status=200)
+            
+        except Exception as err:
+            return JsonResponse({'status': 'Error logging in', 'error': str(err)}, status=400)
+        
+    return render(request, 'login.html')
+            
+                    
+
+
+
+
