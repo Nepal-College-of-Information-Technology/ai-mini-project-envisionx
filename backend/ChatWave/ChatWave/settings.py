50d226ec58015c72b42faf4341e8ee26109f0e64
Aayushman4725 <aayushmannp@gmail.com>
2024-06-17 10:36:11 +0545

    added user model

--- backend/ChatWave/ChatWave/settings.py
+++ backend/ChatWave/ChatWave/settings.py
@@ -115,6 +115,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'Authentication.CustomUser'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
