7a9beb3a5db98ca99eb0e0599e34e9d5aaa15b15
Aayushman4725 <aayushmannp@gmail.com>
2024-06-17 14:09:14 +0545

    added activate path for account activation

--- backend/ChatWave/Authentication/urls.py
+++ backend/ChatWave/Authentication/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('register', registerLogic, name = 'register'),
-    path('login', loginLogic, name='login')
+    path('login', loginLogic, name='login'),
+   path('activate/<uidb64>/<token>',activate, name='activate'),
 ]
