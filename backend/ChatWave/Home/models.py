a11c65a3462bc7256071b2ffc2d4cddea6edf8bf
aryyyn <aryan.211618@ncit.edu.np>
2024-06-19 20:03:05 +0545

    changed some settings

--- backend/ChatWave/Home/models.py
+++ backend/ChatWave/Home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Message(models.Model):
-    text = models.textField()
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
     
