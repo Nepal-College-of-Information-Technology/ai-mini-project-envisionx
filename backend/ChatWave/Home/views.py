10d0db36e23df10ec30b9fe192cca6e6ea3409ad
aryyyn <aryan.211618@ncit.edu.np>
2024-06-24 17:29:45 +0545

    implemented htmx

--- backend/ChatWave/Home/views.py
+++ backend/ChatWave/Home/views.py
@@ -1,24 +1,37 @@
-from django.shortcuts import render, get_object_or_404,redirect
-from .models import *
-from django.contrib.auth.decorators import login_required
-from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import ChatRoom
+from .forms import ChatMessagesForm
 
-
-@login_required()
+# @login_required()
 def homePageLogic(request):
     chat_room = get_object_or_404(ChatRoom, room_name='Room 1')
     chat_messages = chat_room.messages.all()
     form = ChatMessagesForm()
 
-    if request.method == "POST":
+    if request.headers.get('HX-Request'):
+        # This is an HTMX request
         form = ChatMessagesForm(request.POST)
-
-        if form.is_valid():  
+        if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
-            message.room = chat_room 
+            message.room = chat_room
             message.save()
-            return redirect('home')
 
+            # Return JSON response for HTMX request
+            context = {
+                'message': message,
+            }
+            return render(request, 'partials/chat_message_p.html', context)
+
+    else:
+        if request.method == 'POST':
+            form = ChatMessagesForm(request.POST)
+            if form.is_valid():
+                message = form.save(commit=False)
+                message.author = request.user
+                message.room = chat_room
+                message.save()
+                form = ChatMessagesForm()  
 
     return render(request, 'index.html', {'chat_messages': chat_messages, 'form': form})
