adced7c20cceca7ac0292989574d6e9ca93ea37c
aryyyn <aryan.211618@ncit.edu.np>
2024-06-25 13:47:32 +0545

    implemented socket and channel layers

--- backend/ChatWave/ChatWave/asgi.py
+++ backend/ChatWave/ChatWave/asgi.py
@@ -1,28 +1,16 @@
-"""
-ASGI config for ChatWave project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import Home.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatWave.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-django_asgi_app  = get_asgi_application()
-from Home.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter(
-    {
-         "http": django_asgi_app,
-                 "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            Home.routing.websocket_urlpatterns
+        )
+    ),
+})
