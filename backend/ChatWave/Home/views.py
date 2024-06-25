217e640bef8b7f3ee9bde70e4f91f9ec686a1d0b
aryyyn <aryan.211618@ncit.edu.np>
2024-06-25 12:38:43 +0545

    added a working logout button

--- backend/ChatWave/Home/views.py
+++ backend/ChatWave/Home/views.py
@@ -1,32 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from .models import ChatRoom
 from .forms import ChatMessagesForm
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
-@login_required()
+@login_required
 def homePageLogic(request):
     chat_room = get_object_or_404(ChatRoom, room_name='Room 1')
     chat_messages = chat_room.messages.all()
     form = ChatMessagesForm()
 
     if request.headers.get('HX-Request'):
-    
         form = ChatMessagesForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
             message.room = chat_room
             message.save()
-
-           
             context = {
                 'message': message,
             }
             return render(request, 'partials/chat_message_p.html', context)
-
+        else:
+            
+            return JsonResponse({'errors': form.errors}, status=400)
     else:
         if request.method == 'POST':
+            action = request.POST.get('action')
+            if action == 'logout':
+                print("logout clicked!")
+                logout(request)
+                return redirect('login')
             form = ChatMessagesForm(request.POST)
             if form.is_valid():
                 message = form.save(commit=False)
