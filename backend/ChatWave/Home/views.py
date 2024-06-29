e2f2743dfaca19d983a2b4f23d65d0b654c248b2
aryyyn <aryan.211618@ncit.edu.np>
2024-06-29 09:16:30 +0545

    added logic for model

--- backend/ChatWave/Home/views.py
+++ backend/ChatWave/Home/views.py
@@ -4,6 +4,16 @@ from .models import ChatRoom
 from .forms import ChatMessagesForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+import tensorflow as tf
+import json
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import re
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+from keras.preprocessing.sequence import pad_sequences
 
 @login_required
 def homePageLogic(request):
@@ -11,33 +21,107 @@ def homePageLogic(request):
     chat_messages = chat_room.messages.all()
     form = ChatMessagesForm()
 
-    if request.headers.get('HX-Request'):
+    if request.method == 'POST':
+        print(1)
+        action = request.POST.get('action')
+        print(action)
+        if action == 'logout':
+            logout(request)
+            return redirect('login')
+        
         form = ChatMessagesForm(request.POST)
+        print(form)
         if form.is_valid():
+
+            print("2")
+            message_content = form.cleaned_data['body']
+            print(message_content)
+
             message = form.save(commit=False)
+            print(modelTest(message))
+
             message.author = request.user
             message.room = chat_room
             message.save()
-            context = {
-                'message': message,
-                'user': request.user,
-            }
-            return render(request, 'partials/chat_message_p.html', context)
-        else:
-            return JsonResponse({'errors': form.errors}, status=400)
-    else:
-        if request.method == 'POST':
-            action = request.POST.get('action')
-            if action == 'logout':
-                # print("logout clicked!")
-                logout(request)
-                return redirect('login')
-            form = ChatMessagesForm(request.POST)
-            if form.is_valid():
-                message = form.save(commit=False)
-                message.author = request.user
-                message.room = chat_room
-                message.save()
-                form = ChatMessagesForm()
+            
+            form = ChatMessagesForm()
 
     return render(request, 'index.html', {'chat_messages': chat_messages, 'form': form, 'user': request.user})
+
+def modelTest(message):
+    new_model = tf.keras.models.load_model('././model/sentimentmodel.h5')
+    with open('././model/tokenizer.json') as json_file:
+        json_string = json_file.read()
+
+    tokenizer1 = tf.keras.preprocessing.text.tokenizer_from_json(json_string)
+
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    stop_words = stopwords.words('english')
+    stemmer = SnowballStemmer('english')
+    lemmatizer = WordNetLemmatizer()
+
+
+    text_cleaning_re = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+
+    def textcleaning(text, stem=False):
+        tokens = []
+        text = re.sub(text_cleaning_re, ' ', str(text)).strip()
+        
+        for token in text.split():
+            if token not in stop_words:
+                if stem:
+                    tokens.append(stemmer.stem(token))
+                if lemmatizer:
+                    tokens.append(lemmatizer.lemmatize(token))
+                else:
+                    tokens.append(token)
+                    
+                    
+        return " ".join(tokens)
+    
+    text = message
+    words_to_check = ["I", "you", "he", "she", "they", "we", "The", 'the', "I've"]  
+
+    words_in_text = text.split()
+
+
+    filtered_words = [word for word in words_in_text if word not in words_to_check]
+
+
+    new_text = " ".join(filtered_words)
+
+    print(new_text)
+
+    text = new_text
+    list(text)
+    text = textcleaning(text)
+    print(text)
+    tokenizer1.fit_on_texts([text])
+    sequences = tokenizer1.texts_to_sequences([text])
+
+
+    sequences = pad_sequences(sequences, padding='post', maxlen=50)
+
+
+    predictions = new_model.predict(sequences)
+
+    print(predictions)
+    predictions.tolist
+
+    result = ""
+    if (predictions[0][0] > 0.45 and predictions[0][0] < 0.55):
+        result = "calm"
+    else:
+        index = np.argmax(predictions)
+        if (index == 1):
+            result = "sad"
+        else:
+            result = "happy"
+
+    return result
+
+    
+
+                    
+
